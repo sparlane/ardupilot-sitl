fdfb9230c35d6f8e33c67d7df4b3d1691a67cd79
Scott Parlane <scott@parlanenz.com>
2021-02-13 00:43:43 +1300

    Add ability to manage avoidance enable/distance

--- startup.sh
+++ startup.sh
@@ -19,6 +19,18 @@ then
     echo "ADSB_ENABLE	1" >> ${DEFAULTS}
 fi
 
+if [ ! -z "$AVOIDANCE" ]
+then
+    echo "AVD_ENABLE	1" >> ${DEFAULTS}
+    echo "AVD_F_ACTION	4" >> ${DEFAULTS}
+    if [ ! -z "$AVOIDANCE_DIST" ]
+    then
+        AVOIDANCE_DIST=100
+    fi
+    echo "AVD_F_DIST_XY	${AVOIDANCE_DIST}" >> ${DEFAULTS}
+    echo "AVD_F_RCVRY	1" >> ${DEFAULTS}
+fi
+
 if [ ! -z "$BATT_CAPACITY" ]
 then
 	echo "BATT_CAPACITY	${BATT_CAPACITY}" >> ${DEFAULTS}
